84be1ab2ac4c7993373e81ee81bf8401804de57c
Julian Easterling <julian@julianscorner.com>
2023-08-30 22:46:02 +0000

    Had to add some debugging to see runtime behavior of postCreateCommand

--- src/ansible-development/install.sh
+++ src/ansible-development/install.sh
@@ -1,12 +1,26 @@
 #! /usr/bin/env bash
 set -ex
 
-apt-get update -y
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+  echo "Running apt-get update..."
+  apt-get update -y
+fi
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get -y install --no-install-recommends wget ca-certificates vim colordiff
-apt-get -y install --no-install-recommends python3-pip python3-virtualenv
+apt-get install -y --no-install-recommends wget ca-certificates vim colordiff
+
+if ! type git > /dev/null 2>&1; then
+  apt-get install -y --no-install-recommends git
+fi
+
+apt-get install -y --no-install-recommends python3-pip python3-virtualenv
 
 ANSIBLE_VERSION="${VERSION:-latest}";
 
@@ -30,11 +44,13 @@ pip install ansible==${ANSIBLE_VERSION} \
 cat >> /opt/ansible/postCreateCommand.sh << EOF
 #! /usr/bin/env bash
 
-source /opt/ansible/bin/activate
+printenv
 
-cd \${containerWorkspaceFolder}
+export -x ANSIBLE_CONFIG": "\${containerWorkspaceFolder}/ansible.cfg"
 
-echo "Checking '$PWD' for requirements..."
+echo cd \${containerWorkspaceFolder}
+
+echo "Checking '\$PWD' for requirements..."
 if [ -f 'requirements.yml' ]; then
   ansible-galaxy collection install -r requirements.yml
   ansible-galaxy install -r requirements.yml
@@ -44,3 +60,10 @@ EOF
 chmod 0755 /opt/ansible/postCreateCommand.sh
 
 rm -rf /var/lib/apt/lists/*
+
+
+if ! type docker > /dev/null 2>&1; then
+    echo -e '\n(*) Warning: The docker command was not found.\n\nYou can use one of the following scripts to install it:\n\nhttps://github.com/microsoft/vscode-dev-containers/blob/main/script-library/docs/docker-in-docker.md\n\nor\n\nhttps://github.com/microsoft/vscode-dev-containers/blob/main/script-library/docs/docker.md'
+fi
+
+echo 'The 'ansible-development' install script has completed!'
